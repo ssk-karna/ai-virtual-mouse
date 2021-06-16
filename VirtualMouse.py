bf054b8ee45b1eb449d78c69108085c620e16fbd
Satyam <satyamshashankkaran.ssk@gmail.com>
2021-06-16 10:09:34 +0530

    Click and Smoothening

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -7,11 +7,16 @@ import autopy
 wCam,hCam = 648,488
 frameR = 100
 
+cTime = 0
+pTime = 0
+
+smoothening = 7
+plocX,plocY = 0,0
+clocX,clocY = 0,0
+
 cap = cv2.VideoCapture(0)
 cap.set(3,wCam)
 cap.set(4,hCam)
-cTime = 0
-pTime = 0
 
 wScr, hScr = autopy.screen.size()
 
@@ -38,15 +43,28 @@ while True:
             #5. Convert coordinates
             x3 = np.interp(x1,(frameR,wCam-frameR),(0,wScr))
             y3 = np.interp(y1,(frameR,hCam-frameR),(0,hScr))
-
             
             #6. Smoothen values
+
+            clocX = plocX + (x3-plocX)/smoothening
+            clocY = plocY + (y3-plocY)/smoothening
+
             #7. Move mouse
-            autopy.mouse.move(wScr-x3,y3)
+            autopy.mouse.move(wScr-clocX,clocY)
             cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
+            plocX,plocY = clocX,clocY
+
         #8. Both index and middle fingers are up
+        if fingers[1]==1 and fingers[2]==1:
+
         #9. Find Distance between fingers
+            length,img,lineInfo = detector.findDistance(8,12,img)
+            #print(length)
         #10. Click mouse if distance short
+            if length < 25:
+                cv2.circle(img,(lineInfo[4],lineInfo[5]),15,(0,255,0),cv2.FILLED)
+                autopy.mouse.click()
+            
 
         #11. Frame Rate
         
