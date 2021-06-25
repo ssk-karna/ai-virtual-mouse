bd9aed89216f4e1313a7195f27c2a7c6978ee9cd
Satyam <satyamshashankkaran.ssk@gmail.com>
2021-06-25 11:43:01 +0530

    Mirror img

--- VirtualMouse.py
+++ VirtualMouse.py
@@ -77,21 +77,18 @@ while True:
                 
             
             autopy.mouse.toggle(down = False)
-            
-                
-        
-        
-
+   
         #11. Frame Rate
         
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
 
+    img=cv2.flip(img,1)
+    
     cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,
     (255,0,255),3)
     #12. Display
-
-
+    
     cv2.imshow("Image",img)
     cv2.waitKey(1)
